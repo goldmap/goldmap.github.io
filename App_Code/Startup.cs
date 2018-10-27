3870ef8568e108eeef4074df9b7242c4ab3b85c5
goldmap <42493201+goldmap@users.noreply.github.com>
2018-10-27 09:24:30 +0800

    Test

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JWebSite.Startup))]
+namespace JWebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
